9e4b00d6d3b989d4721b4f4265543762b15a740f
Albern79 <albern79@gmail.com>
2024-04-11 23:57:19 +0700

    add plain test

--- tests/Test.php
+++ tests/Test.php
@@ -16,11 +16,13 @@ class Test extends TestCase
         $t2path4 = __DIR__ . "/fixtures/file4.json";
         $yamlPath1 = __DIR__ . "/fixtures/file1.yaml";
         $yamlPath2 = __DIR__ . "/fixtures/file2.yaml";
-        $shouldBe1 = rtrim(file_get_contents(__DIR__ . "/fixtures/testResult1.txt", 0, null, null), "\r\n");
+        $jsonStylishFormat = rtrim(file_get_contents(__DIR__ . "/fixtures/testResult1.txt", 0, null, null), "\r\n");
         $shouldBe2 = rtrim(file_get_contents(__DIR__ . "/fixtures/testResult2.txt", 0, null, null), "\r\n");
-        $shouldBe3 = rtrim(file_get_contents(__DIR__ . "/fixtures/testYamlResult.txt", 0, null, null), "\r\n");
-        $this->assertEquals($shouldBe1, gendiff($t1path1, $t1path2, 'stylish'));
+        $yamlFormat = rtrim(file_get_contents(__DIR__ . "/fixtures/testYamlResult.txt", 0, null, null), "\r\n");
+        $PlainFormat= rtrim(file_get_contents(__DIR__ . "/fixtures/testPlainResult.txt", 0, null, null), "\r\n");
+        $this->assertEquals($jsonStylishFormat, gendiff($t1path1, $t1path2, 'stylish'));
         $this->assertEquals($shouldBe2, gendiff($t2path3, $t2path4, 'stylish'));
-        $this->assertEquals($shouldBe3, gendiff($yamlPath1, $yamlPath2, 'stylish'));
+        $this->assertEquals($yamlFormat, gendiff($yamlPath1, $yamlPath2, 'stylish'));
+        $this->assertEquals($PlainFormat, gendiff($t2path3, $t2path4, 'plain'));
     }
 }
